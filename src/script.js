ef6ae759e464a5daff7ae047ebb36cb99594dd8b
hang-on <hang-on@users.noreply.github.com>
2025-01-28 19:37:38 +0100

    Update canvas size and implement FPS display in game loop

--- src/script.js
+++ src/script.js
@@ -1,19 +1,33 @@
+
+let secondsPassed;
+let oldTimeStamp;
+let fps;
+
+function gameLoop(timeStamp) {
+
+    // Calculate the number of seconds passed since the last frame
+    secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+
+    // Calculate fps
+    fps = Math.round(1 / secondsPassed);
+
+    // Draw number to the screen
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, 200, 100);
+    context.font = '25px Arial';
+    context.fillStyle = 'black';
+    context.fillText("FPS: " + fps, 10, 30);
+
+    // Perform the drawing operation
+    draw();
+
+    // The loop function has reached it's end. Keep requesting new frames
+    window.requestAnimationFrame(gameLoop);
+}
+
 window.onload = function() {
     const canvas = document.getElementById('gameCanvas');
     const context = canvas.getContext('2d');
 
-    // Load sprite image
-    const sprite = new Image();
-    sprite.src = 'gfx/longmoan.png'; // Update with the path to your sprite image
-
-    const tyris = new Image();
-    tyris.src = 'gfx/tyris.png'; // Update with the path to your sprite image
-
-
-    sprite.onload = function() {
-        // Draw sprite on canvas
-        context.drawImage(sprite, 0, 0);
-        context.drawImage(tyris, 450, 355);
-                
-    };
 };
